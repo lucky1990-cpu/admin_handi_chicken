335808d453c53945ef334729b44ad90864b92253
Kumar <lucky.a.kumar@accenture.com>
2021-11-23 18:39:07 +0530

    add

--- public/js/BiryaniMahalEditItems.js
+++ public/js/BiryaniMahalEditItems.js
@@ -30,56 +30,7 @@ $.ajax({
 
   }
 
-  document.querySelector('#UpdateFood').addEventListener('click',(e)=>{
-    e.preventDefault();
-    const updatedFood = {
-      ID:document.getElementById('EditBiryaniMahalFoodID').innerHTML,
-      FoodNmae :document.getElementById('EditBiryaniMahalFoodName').value,
-      Amount:  document.getElementById('EditBiryaniMahalFoodAmount').value,
-      foodDescription:document.getElementById('foodBiryaniMahalDescription').value
+ 
   
-  }
-   UpdateFoodItem(updatedFood)
-   })
   
-   document.querySelector('#DeleteFood').addEventListener('click',(e)=>{
-    e.preventDefault();
-    const deleteId = document.getElementById('EditBiryaniMahalFoodID').innerHTML;
-    DeleteRecord({Id:deleteId})
-  
-   })
-   function DeleteRecord(deleteId){
-     const delData =  JSON.stringify(deleteId);
-    $.ajax({
-      type: "POST",
-      url: URLDeleteFood,
-      dataType: "json",
-     contentType: "application/json; charset=utf-8",
-     data: delData,
-      success: function(data){
-        alert(data.data)
-        window.location.href='/'
-      },
-      error:function(e){
-        console.log(e)
-      }
-    });
-   }
-  
-   function UpdateFoodItem(updatedFood){
-     const EditData = JSON.stringify(updatedFood)
-    $.ajax({
-      type: "POST",
-      url: UpdateFood,
-      dataType: "json",
-     contentType: "application/json; charset=utf-8",
-     data: EditData,
-      success: function(data){
-        alert(data.data)
-      },
-      error:function(e){
-        console.log(e)
-      }
-    });
-  
-   }
+ 
