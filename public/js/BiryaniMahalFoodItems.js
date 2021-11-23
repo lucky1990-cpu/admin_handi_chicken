91f048ad6f1128d632cddd26685157c3509fc065
Kumar <lucky.a.kumar@accenture.com>
2021-11-23 15:44:12 +0530

    add

--- public/js/BiryaniMahalFoodItems.js
+++ public/js/BiryaniMahalFoodItems.js
@@ -5,6 +5,7 @@ const onAddNewFoodItems = ()=>{
 }
 $.get(BiryaniFoodItemsURL,(result)=>{
     BiryaniFoodBinding(result)
+    console.log(result)
     document.querySelector('.loader').style.visibility='hidden';
 })
 
@@ -16,31 +17,35 @@ $.get(BiryaniFoodItemsURL,(result)=>{
 
  const unOrderList = document.querySelector('BiryaniMahalFoodContainer');
 const BiryaniFoodBinding = (data)=>{
+    data.forEach(element => {
+        const listItem = document.createElement('li');
+        listItem.className='list-group-item d-flex justify-content-between align-items-start'
+        const div1 = document.createElement('div');
+        div1.className='ms-2 me-auto';
+        const div2 = document.createElement('div')
+        div2.className='fw-bold';
+        div2.innerHTML=element.foodItem;
+        div1.appendChild(div2);
+        const per = document.createElement('p');
+        const bold = document.createElement('b');
+        bold.innerHTML='RS:' +element.Amount
+        per.appendChild(bold)
+        div1.appendChild(per)
+        const foodButton = document.createElement('button')
+        foodButton.className='btn btn-primary';
+        foodButton.innerHTML='Order Now'
+        div1.appendChild(foodButton)
+        listItem.appendChild(div1);
+        const foodImg = document.createElement('img')
+        foodImg.className='img-fluid rounded'
+        foodImg.src='Img/Restaurant.jpg'
+        foodImg.style.width='8rem';
+        listItem.appendChild(foodImg)
+        unOrderList.appendChild(listItem)
+    });
     
-      const listItem = document.createElement('li');
-      listItem.className='list-group-item d-flex justify-content-between align-items-start'
-      const div1 = document.createElement('div');
-      dev1.className='ms-2 me-auto';
-      const div2 = document.createElement('div')
-      div2.className='fw-bold';
-      div2.innerHTML='Chicken Biryani';
-      div1.appendChild(div2);
-      const per = document.createElement('p');
-      const bold = document.createElement('b');
-      bold.innerHTML='RS:140'
-      per.appendChild(bold)
-      div1.appendChild(per)
-      const foodButton = document.createElement('button')
-      foodButton.className='btn btn-primary';
-      foodButton.innerHTML='Order Now'
-      div1.appendChild(foodButton)
-      listItem.appendChild(div1);
-      const foodImg = document.createElement('img')
-      foodImg.className='img-fluid rounded'
-      foodImg.src='Img/Restaurant.jpg'
-      foodImg.style.width='8rem';
-      listItem.appendChild(foodImg)
-      unOrderList.appendChild(listItem)
+     
+     
 
 
 
